9cbe3145e0bba92e08b65adb02c9c72eece8a9af
mark medlin <mmedlin1997@yahoo.com>
2016-06-16 09:47:12 -0700

    The original commit for the Hello project

---
+++ Foothill.java
@@ -0,0 +1,12 @@
+
+public class Foothill
+{
+   public static void main(String args[])
+   {
+      System.out.println("Hello Eclipse!");
+   }
+}
+
+/* -------------- paste of run from console window -------------------------
+Hello Eclipse!
+---------------------------------------------------------------------------- */
