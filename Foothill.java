b47bf0f209cda3c8e02b8ae28ce44d945a638ea1
mark medlin <mmedlin1997@yahoo.com>
2016-06-16 18:23:00 -0700

    Changed comment

--- Foothill.java
+++ Foothill.java
@@ -3,7 +3,7 @@ public class Foothill
 {
    public static void main(String args[])
    {
-      System.out.println("Hello Eclipse!");
+      System.out.println("Hello Eclipse! Let's make a change...");
    }
 }
 
